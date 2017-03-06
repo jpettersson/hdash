1737eccd3e6f48308d389ef9416af16ed6de107c
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2017-03-06 15:31:17 +0100

    'Fixing' unit tests..

--- app/api/heroic.test.ts
+++ app/api/heroic.test.ts
@@ -5,14 +5,17 @@ import { assertModel } from './test-utils';
 describe('heroic', () => {
   it('should handle Sampling', () => {
     assertModel(heroic.SumAggregation, {
-      sampling: { size: 42, unit: "seconds" }
+      sampling: { size: 42, unit: 1 }
     }, { sampling: null });
   });
 
   it('should handle Query', () => {
     assertModel(heroic.Query, {
       range: { type: "absolute", start: 0, end: 1000 },
-      query: "average by host"
+      query: "average by host",
+      options: {
+        ticksGoal: 1
+      }
     }, { query: "foo" });
   });
 });
